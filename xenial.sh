2e6143e9e5a34935ec295e85557792723c3b4c14
Roman Yurchik <ryurchik@akademos.com>
2018-03-11 22:48:12 +0200

    typo

--- xenial.sh
+++ xenial.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y puppet
 
 cd /etc
 sudo mv puppet/ puppet-bak
-git clone https://github.com/tijsverwest/tma-puppet.git /etc/puppet
+sudo git clone https://github.com/tijsverwest/tma-puppet.git /etc/puppet
 
 cd /etc/puppet
 sudo puppet apply /etc/puppet/manifests/site.pp
