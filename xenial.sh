0db33038a6151c4e120c2a0d38730758f44c9244
Roman Yurchik <ryurchik@akademos.com>
2018-03-12 16:12:13 +0200

    ansible instead of puppet

--- xenial.sh
+++ xenial.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
-cd /tmp
-wget https://apt.puppetlabs.com/puppet5-release-xenial.deb
-sudo dpkg -i puppet5-release-xenial.deb
-sudo apt update
-sudo apt-get install -y puppet
-
-cd /etc
-sudo mv puppet/ puppet-bak
-sudo git clone https://github.com/tijsverwest/tma-puppet.git /etc/puppet
-
-cd /etc/puppet
-sudo puppet apply /etc/puppet/manifests/site.pp
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible
